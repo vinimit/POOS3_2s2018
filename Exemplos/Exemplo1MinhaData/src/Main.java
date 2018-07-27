f0950c27df302f9bf8c76abdf26e2c440faf3f67
Ednilson Geraldo Rossi <ednilsonrossi@gmail.com>
2018-07-27 11:04:41 -0300

    Aula de 27/07/18 atualizada no laboratório

--- Exemplos/Exemplo1MinhaData/src/Main.java
+++ Exemplos/Exemplo1MinhaData/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		MinhaData hoje;
 
-        hoje = new MinhaData(27, 07, 2018);
+        hoje = new MinhaData(27, 17, 2018);
 
         System.out.println("Hoje no Brasil.: " + hoje.dataBrazil());
         System.out.println("Hoje nos EUA...: " + hoje.dataUS());
--- Exemplos/Exemplo1MinhaData/src/MinhaData.java
+++ Exemplos/Exemplo1MinhaData/src/MinhaData.java
@@ -5,9 +5,32 @@ public class MinhaData {
     private int ano;
 
     public MinhaData(int oDia, int oMes, int oAno){
-        dia = oDia;
-        mes = oMes;
-        ano = oAno;
+        if(ehValida(oDia, oMes, oAno)){
+            dia = oDia;
+            mes = oMes;
+            ano = oAno;
+        }else{
+            dia = 1;
+            mes = 1;
+            ano = 1900;            
+        }
+    }
+
+    private boolean ehValida(int oDia, int oMes, int oAno){
+        boolean result = false;
+        
+        if(oDia >= 1 && oDia <= 31){
+            result = true;
+        }
+
+        if(result && (oMes < 1 || oMes > 12)){
+            result = false;
+        } 
+
+        if(result && oAno < 0){
+            result = false;
+        }        
+        return result;
     }
 
     public String dataBrazil(){
